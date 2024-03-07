71894271f6d40be21db2f7560fea8fed259a0662
Luis Trinta <luismiguelferreiratrinta@gmail.com>
2024-03-07 00:57:53 +0000

    Innitial commit
    Added the initial structure, as well as simple API requests to 2 of the providers. Also created a database folder where available files for tables /postgres setup will be located

---
+++ README.md
@@ -0,0 +1,7 @@
+# Weather aggregator
+This simple project showcases API interactions, ETL (Extract Transform Load) logic applied and relational databases (PostgreSQL)
+
+## API's used for this project
+- https://www.tomorrow.io
+- https://openweathermap.org
+
---
+++ weather-aggregator.py
@@ -0,0 +1,21 @@
+import requests 
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+
+def get_current_weather(api_name,location):
+    if api_name == "TOMORROW_IO":
+        api_key = os.getenv(api_name)
+        tomorrow_io = requests.get(f'https://api.tomorrow.io/v4/weather/realtime?location={location}&apikey={api_key}')
+        print(tomorrow_io.text)
+    elif api_name == "OPEN_WEATHER":
+        api_key = os.getenv(api_name)
+        open_weather = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={location}&appid={api_key}')
+        print(open_weather.text)
+
+print("TOMORROW_API")
+get_current_weather("TOMORROW_IO","Lisbon")
+print("\n\n")
+print("OPEN WEATHER")
+get_current_weather("OPEN_WEATHER","Lisbon")
