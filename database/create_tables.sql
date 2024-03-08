49ed26fd6ad65ee8822f1463ca48bc40d2821e8a
Luis Trinta <luismiguelferreiratrinta@gmail.com>
2024-03-08 00:03:17 +0000

    Database update + new function
    Updated the database files for the new table structure and create a dummy function to test the psycopg2 library for postgres interaction

--- database/create_tables.sql
+++ database/create_tables.sql
@@ -1,9 +1,10 @@
 drop table if exists TOMORROW_IO;
 drop table if exists OPEN_WEATHER;
+drop table if exists MERGED_WEATHER_DATA;
 
 create table TOMORROW_IO (
 id integer primary key,
-date date,
+date timestamptz,
 location varchar(30),
 temperature float,
 apparent_temp float,
@@ -21,7 +22,26 @@ uv_value integer
 
 create table OPEN_WEATHER (
 id integer primary key,
-date date,
+date timestamptz,
+location varchar(30),
+temperature float,
+temperature_min float,
+temperature_max float,
+apparent_temp float,
+pressure float,
+humidity float,
+wind_dir float,
+wind_gust float,
+wind_speed float,
+visibility float,
+weather_status varchar(40),
+sunset_date timestamptz,
+sunrise_date timestamptz
+);
+
+create table MERGED_WEATHER_DATA (
+id integer primary key,
+date timestamptz,
 location varchar(30),
 temperature float,
 temperature_min float,
@@ -35,6 +55,8 @@ wind_speed float,
 visibility float,
 precipitation float,
 weather_status varchar(40),
-sunset_date date,
-sunrise_date date
+sunset_date timestamptz,
+sunrise_date timestamptz,
+rain_intensity float,
+uv_value integer
 );
--- weather-aggregator.py
+++ weather-aggregator.py
@@ -1,8 +1,15 @@
 import requests 
 from dotenv import load_dotenv
 import os
-load_dotenv()
+import psycopg2
 
+load_dotenv()
+conn = psycopg2.connect(database=os.getenv("DATABASE"),
+                                 host=os.getenv("HOST"),
+                                 user=os.getenv("USER"),
+                                 password=os.getenv("PASSWORD"),
+                                 port=os.getenv("PORT")
+)
 
 def get_current_weather(api_name,location):
     if api_name == "TOMORROW_IO":
@@ -17,8 +24,12 @@ def get_current_weather(api_name,location):
         open_weather = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={location}&appid={api_key}')
         print(open_weather.text)
 
+def load_database(payload,table_name):
+    cursor = conn.cursor()
+    cursor.execute(f"SELECT * FROM {table_name.lower()}")
+    print(cursor.fetchone())
 print("TOMORROW_API")
 get_current_weather("TOMORROW_IO","Porto")
-print("\n\n")
-print("OPEN WEATHER")
+
 get_current_weather("OPEN_WEATHER","Porto")
+load_database({},"TOMORROW_IO")
